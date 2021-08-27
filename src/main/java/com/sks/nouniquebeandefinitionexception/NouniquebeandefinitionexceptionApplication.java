dda0a6cc4aeba10191c5f0c6c89f78a6fa0042fc
skeres <stephane.keresztes@gmail.com>
2021-08-27 09:59:08 +0200

    initial

---
+++ src/main/java/com/sks/nouniquebeandefinitionexception/NouniquebeandefinitionexceptionApplication.java
@@ -0,0 +1,13 @@
+package com.sks.nouniquebeandefinitionexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NouniquebeandefinitionexceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NouniquebeandefinitionexceptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sks/nouniquebeandefinitionexception/NouniquebeandefinitionexceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sks.nouniquebeandefinitionexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NouniquebeandefinitionexceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
